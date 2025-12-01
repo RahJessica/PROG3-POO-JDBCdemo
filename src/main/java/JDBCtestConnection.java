a437749e14c1e8aac247297a1ae8998d49d50a2a
RahJess <jessicakantotiana@gmail.com>
2025-12-01 11:01:40 +0100

    Implement JDBC connection logic using DriverManager and create a simple database postgresql

---
+++ src/main/java/JDBCtestConnection.java
@@ -0,0 +1,21 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+// ouverture d'une session de connexion à la base de données postgresql
+public class JDBCtestConnection {
+    public static void main(String[] args) {
+        String url  = "jdbc:postgresql://localhost:5432/postgres";
+        String user = "postgres";
+        String password = "userSQL$";
+
+        try {
+            Connection conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Connexion réussie !");
+            conn.close();
+        }
+        // récupération des erreurs dans le try et affiche les détails dans la console
+            catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/ToDo.java
+++ src/main/java/ToDo.java
@@ -1,3 +1,17 @@
-public class ToDo {
+import java.time.Instant;
 
+public class ToDo {
+    private final Integer id;
+    private final String title;
+    private final Instant dateTime;
+    private final String description;
+    private final Boolean isCompleted;
+
+    public ToDo(Integer id, String title, Instant dateTime, String description, Boolean isCompleted) {
+        this.id = id;
+        this.title = title;
+        this.dateTime = dateTime;
+        this.description = description;
+        this.isCompleted = isCompleted;
+    }
 }
